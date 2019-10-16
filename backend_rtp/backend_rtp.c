6a4aecd0cc7f1cfd156c1e793fa62fefeb1b098e
JonathanS <mail@strfry.org>
2019-10-16 23:18:00 +0200

    properly assemble packets

--- backend_rtp/backend_rtp.c
+++ backend_rtp/backend_rtp.c
@@ -23,27 +23,26 @@
 #include <arpa/inet.h>
 
 #define SAP_MULTICAST_GROUP "sap.mcast.net"
+//#define SAP_MULTICAST_GROUP "224.2.127.254"
 #define SAP_MULTICAST_PORT 9875
 
-//#define PULSEAUDIO_MULTICAST_GROUP "224.0.0.56"
-#define PULSEAUDIO_MULTICAST_GROUP "224.2.127.254"
+#define PULSEAUDIO_MULTICAST_GROUP "224.0.0.56"
 
 #define MAGIC_RTP_PORT  12077
 
 
 const char* simple_sdp = 
 "v=0\n"
-"o=pulse 3779552618 0 IN IP4 172.16.42.2\n"
+"o=root %ld 0 IN IP4 172.16.42.2\n"
 "s=VirtualOSS RTP Stream on thor.strfry.org\n"
 "c=IN IP4 224.0.0.56\n"
-"t=3779552618 0\n"
+"t=%lu 0\n"
 "a=recvonly\n"
 "m=audio 12077 RTP/AVP 10\n"
 "a=rtpmap:10 L16/44100/2\n"
 "a=type:broadcast\n"
 ;
 
-uint16_t rtp_port = MAGIC_RTP_PORT;
 
 static void
 rtp_close(struct voss_backend *pbe)
@@ -71,6 +70,12 @@ rtp_open(struct voss_backend *pbe, const char *devname,
 		 IP_MULTICAST_LOOP,
 		 &loop,
 		 sizeof(loop));
+         
+   unsigned char ttl = 255;
+   setsockopt(pbe->fd, IPPROTO_IP,
+		 IP_MULTICAST_TTL,
+		 &ttl,
+		 sizeof(ttl));
 
     bzero((char *)&addr, sizeof(addr));
     addr.sin_family = AF_INET;
@@ -87,12 +92,20 @@ rtp_open(struct voss_backend *pbe, const char *devname,
 
 
     connect(pbe->fd, (struct sockaddr*)&addr, addrlen);
-    send_sap(pbe->fd, simple_sdp, 0);
+
+    char buffer[1024];
+
+    uint64_t timestamp = time(0) + 2208988800ULL;
+
+    sprintf(buffer, simple_sdp, timestamp, timestamp);
+    
+    send_sap(pbe->fd, buffer, 0);
+    //send_sap(pbe->fd, simple_sdp, 0);
     
     bzero((char *)&addr, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr(PULSEAUDIO_MULTICAST_GROUP);
-    addr.sin_port = htons(rtp_port);
+    addr.sin_port = htons(MAGIC_RTP_PORT);
     addrlen = sizeof(addr);
 
     connect(pbe->fd, (struct sockaddr*)&addr, addrlen);
@@ -117,31 +130,43 @@ rtp_open(struct voss_backend *pbe, const char *devname,
 
 uint32_t timestamp = 0;
 uint16_t seqnum = 0;
-uint32_t ssrc = 3779552618U;
+uint32_t ssrc = 3780246724U;
+
+const int MAX_MTU = 1400;
 
 static int
 rtp_play_transfer(struct voss_backend *pbe, void *ptr, int len)
 {
-    uint8_t* rtp_header8 = ptr;
-    uint16_t* rtp_header16 = ptr;
-    uint32_t* rtp_header32 = ptr;
-
+   // printf("rtp_play_transfer(len=%d)\n", len);
+    
+    uint8_t pkg[MAX_MTU];
+    
+    const int hdr_len = 12;
+    
+    len = len > (MAX_MTU - hdr_len) ? (MAX_MTU - hdr_len) : len;
+    len = len & (~0x3); // full frames
+    
     int samples = len / sizeof(int16_t) / 2/*->channels*/;
-    //timestamp += samples;
+    timestamp += samples;
+    
+    uint8_t* rtp_header8 = pkg;
+    uint16_t* rtp_header16 = pkg;
+    uint32_t* rtp_header32 = pkg;
+
+    rtp_header8[0] = 2 << 6;
+    rtp_header8[1] = 10; // PAYLOAD type 10: L16/44100
     
     rtp_header16[1] = seqnum++;
 
-    rtp_header8[0] = 2 << 6;
-    rtp_header8[1] = 10; // PAYLOAD t
-    rtp_header8[2] = 0;
-    rtp_header8[3] = 0;
-
     rtp_header32[1] = timestamp;
+    // rtp_header32[1] = 0; // no timestamp makes pulseaudio guess the sample rate
     rtp_header32[2] = ssrc;
 
-    timestamp += len;
 
-    return send(pbe->fd, ptr, 1024, 0);
+    memcpy(pkg + hdr_len, ptr, len);
+
+    send(pbe->fd, pkg, len + hdr_len, 0);
+    return len;
 }
 
 static void
